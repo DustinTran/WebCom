ff84a6b183f9e6f04c532291a9001233b1fd9b65
francois37 <francois.senis@gmail.com>
2013-01-26 11:56:31 +0100

    Ajout de l'enregistrement d'un utilisateur
    + une partie du menu

--- src/com/projet/webcom/AccueilActivity.java
+++ src/com/projet/webcom/AccueilActivity.java
@@ -3,32 +3,42 @@ package com.projet.webcom;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 
-public class AccueilActivity extends Activity {
+public class AccueilActivity extends Activity
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accueil);
-		
-		ExpandableListView  listViewPrincipal = (ExpandableListView) findViewById(R.id.expandableListView1);
-		
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.ListePrincipale, android.R.layout.simple_expandable_list_item_1);
-		
-		adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-		
-		listViewPrincipal.setAdapter(adapter);
-		//fichier de test commit
+
+//		ExpandableListView listViewPrincipal = (ExpandableListView) findViewById(R.id.expandableListView1);
+//
+//		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ListePrincipale,
+//				android.R.layout.simple_spinner_item);
+//
+//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//		listViewPrincipal.setAdapter(adapter);
+//
+//		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//		spinner.setOnItemSelectedListener(this);
+//		// fichier de test commit
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_accueil, menu);
-		return true;
+	public boolean onCreateOptionsMenu(Menu menu){
+	    super.onCreateOptionsMenu(menu);
+	    MenuItem item = menu.add("Accueil");
+	    item.setIcon(R.drawable.ic_menu_accueil);
+	    item = menu.add("Panier");
+	    item.setIcon(R.drawable.ic_menu_panier);
+	    
+	    return true;
 	}
 
 }
